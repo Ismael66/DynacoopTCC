44b1d40653915e9d38e92635d8f3492a67c919b8
Ismael Mateus <ismaelbnt66@gmail.com>
2022-08-19 17:30:42 -0300

    Plugin clona oportunidade LV

---
+++ Plugins/Ambiente1/ClonaOportunidade.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Xrm.Sdk;
+using Plugins.Models;
+using Plugins.Utilidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Uteis.Implementos;
+
+namespace Plugins.Ambiente1
+{
+    public class ClonaOportunidade : PluginImplement
+    {
+        public override void ExecutePlugin(IServiceProvider serviceProvider)
+        {
+            IOrganizationService serviceAmbienteDois = Conexao.GetService();
+            Oportunidade opp = new Oportunidade();
+
+
+            if (Context.MessageName == "Create")
+            {
+                var opportunity = (Entity)Context.InputParameters["Target"];
+                var integrationOpportunity = new Entity("opportunity");
+
+                foreach (var field in opportunity.Attributes)
+                {
+                    if (field.Value != null)
+                    {
+                        if (field.Value.GetType() == new EntityReference().GetType())
+                        {
+                            if (!opp.getIgnoreFields().ToList().Contains(((EntityReference)field.Value).LogicalName) && field.Key != $"{((EntityReference)field.Value).LogicalName}id")
+                                integrationOpportunity[field.Key] = opp.ValidateLookup(field, Service, serviceAmbienteDois);
+                        }
+                        else
+                        {
+                            integrationOpportunity[field.Key] = field.Value;
+                        }
+                    }
+                }
+
+                integrationOpportunity["dyp2_integracao"] = true;
+
+                serviceAmbienteDois.Create(integrationOpportunity);
+            }
+        }
+    }
+}
