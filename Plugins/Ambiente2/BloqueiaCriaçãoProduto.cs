207099a72878b227af1d31ddeafec172b6b554c1
Ismael Mateus <ismaelbnt66@gmail.com>
2022-08-19 00:06:52 -0300

    "Validação criação de produto"

--- Plugins/Ambiente2/BloqueiaCriaçãoProduto.cs
+++ Plugins/Ambiente2/BloqueiaCriaçãoProduto.cs
@@ -8,8 +8,17 @@ namespace Plugins.Ambiente2
     {
         public override void ExecutePlugin(IServiceProvider serviceProvider)
         {
-            throw new InvalidPluginExecutionException("Não é possivel criar produto no ambiente Dynamics2, " +
-                                                        "a criação de produtos é permitida somente no ambiente Dynamics1");
+            if (this.Context.InputParameters.Contains("Target") &&
+                this.Context.InputParameters["Target"] is Entity)
+            {
+                Entity produto = (Entity)this.Context.InputParameters["Target"];
+                bool bloqueaCriacao = produto.GetAttributeValue<bool>("log2_bloquearcriacao");
+                if (bloqueaCriacao)
+                {
+                    throw new InvalidPluginExecutionException("Não é possivel criar produto no ambiente Ambiente 2, " +
+                                                        "a criação de produtos é permitida somente no ambiente Ambiente 1");
+                }
+            }
         }
     }
 }
